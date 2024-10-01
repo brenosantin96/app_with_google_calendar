2d37f6fec092bdda0ad3a48a37a1916c56aabb6a
Breno Santin <brenolimaa96@gmail.com>
2024-10-01 21:09:02 +0200

    first commit

---
+++ src/app/(private)/events/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const EventsPage = () => {
+    return (
+        <>
+            <h1>Events</h1>
+            <UserButton />
+        </>
+    )
+}
+
+export default EventsPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from "next";
+import { Inter } from 'next/font/google'
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'], variable: "--font-sans" })
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: "./fonts/GeistVF.woff", // Define o caminho local do arquivo de fonte
+  variable: "--font-geist-sans", // Cria uma variável CSS para usar a fonte
+  weight: "100 900", // Define os pesos permitidos para a fonte, de 100 a 900
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -24,12 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("min-h-screen, bg-background, font-sans, antialiased", inter.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
